64a24d055775165f73f2c5f50050911c57f644fe
Semesse <Semperia@outlook.com>
2022-04-02 07:57:44 +0000

    test

--- src/helper.rs
+++ src/helper.rs
@@ -3,6 +3,6 @@ use std::time::{SystemTime, UNIX_EPOCH};
 pub fn get_current_nanos() -> u128 {
     SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .expect("SystemTimeError")
+        .expect("SystemTime")
         .as_nanos()
 }
